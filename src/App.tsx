d551dfe2677eda8e1c13ad4070af9f4efa76409b
blove <blove@users.noreply.github.com>
2025-06-02 17:22:38 -0700

    03-challenge-pie-chart

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ enum ChartType {
 }
 
 function App() {
-  const [chartType, setChartType] = useState<ChartType>(ChartType.Bar);
+  const [chartType, setChartType] = useState<ChartType>(ChartType.Pie);
 
   const handleChartTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
     setChartType(event.target.value as ChartType);
